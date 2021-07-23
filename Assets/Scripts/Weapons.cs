d690a3ae4e6aee0859b60ea4578736a27a6d1154
tanasescu-robert98 <80208723+tanasescu-robert98@users.noreply.github.com>
2021-07-23 11:06:21 +0300

    added weapon script

---
+++ Assets/Scripts/Weapons.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapons : MonoBehaviour
+{
+    public GameObject Player;
+    public static bool melee_weapon_equiped = true;
+    public static bool range_weapon_equiped = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
