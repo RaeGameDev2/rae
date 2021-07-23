d6167c8bea29766e5eaec880d1d375860a5371d4
AndreeaMoruz <adamoruz@yahoo.com>
2021-07-23 15:51:20 +0300

    Revert "Merge branch 'master' of https://github.com/SaiTatter133/rae"
    This reverts commit 50beb99dcd8b29f119a8061a7c3898cae71939e7, reversing
    changes made to 96cd33a61536b196d1ceb498e4260be5e5929056.

--- Assets/Scripts/Weapons.cs
+++ Assets/Scripts/Weapons.cs
@@ -1,46 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Weapons : MonoBehaviour
 {
-    //public GameObject Player;
-    public GameObject Sword;
-    public GameObject Bow;
-    public Text Text;
+    public GameObject Player;
     public static bool melee_weapon_equiped = true;
     public static bool range_weapon_equiped = false;
     // Start is called before the first frame update
     void Start()
     {
-        melee_weapon_equiped = true;
-        range_weapon_equiped = false;
-        Sword.gameObject.SetActive(true);
-        Bow.gameObject.SetActive(false);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.T))
-        {
-            if(melee_weapon_equiped == true)
-            {
-                Text.text = "Bow equipped!";
-                melee_weapon_equiped = false;
-                range_weapon_equiped = true;
-                Sword.gameObject.SetActive(false);
-                Bow.gameObject.SetActive(true);
-            }
-            else if(range_weapon_equiped == true)
-            {
-                Text.text = "Sword equipped!";
-                melee_weapon_equiped = true;
-                range_weapon_equiped = false;
-                Sword.gameObject.SetActive(true);
-                Bow.gameObject.SetActive(false);
-            }
-        }
+        
     }
 }
