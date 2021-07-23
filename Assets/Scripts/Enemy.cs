1f109510ff23dfa3dae8b3c3f3761cb05d72c60b
tanasescu-robert98 <80208723+tanasescu-robert98@users.noreply.github.com>
2021-07-23 16:10:00 +0300

    a

--- Assets/Scripts/Enemy.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Enemy : MonoBehaviour
-{
-    public float hitPoints = 100f;  // poate fi modificat din inspector dupa nevoie
-}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,44 @@
-using System.Collections;
-using System.Collections.Generic;
+<<<<<<< HEAD
+=======
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    void Start()
+        [SerializeField] private float speed;
+        private Rigidbody2D body;
+        private bool grounded;
+
+    private void Awake()
     {
-        
+        body = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
+    private void Update()
     {
-        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+        //Flip player when moving left-right
+        if (horizontalInput > 0.01f)
+            transform.localScale = new Vector3(5, 5, 5);//pune in paranteza dimensiunile obiectului 
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector3(-5, 5, 5);
+
+        //pentru saritura:
+        if (Input.GetKey(KeyCode.Space) && grounded)
+            Jump();
+    }
+
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, speed);
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            grounded = true;
     }
 }
+>>>>>>> parent of 50beb99 (Merge branch 'master' of https://github.com/SaiTatter133/rae)
