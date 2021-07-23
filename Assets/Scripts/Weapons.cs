78225c13924e0baabe5f2497abf54b36443b035a
tanasescu-robert98 <80208723+tanasescu-robert98@users.noreply.github.com>
2021-07-23 12:52:02 +0300

    modified weapons script

--- Assets/Scripts/Weapons.cs
+++ Assets/Scripts/Weapons.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Weapons : MonoBehaviour
 {
     //public GameObject Player;
     public GameObject Sword;
     public GameObject Bow;
+    public Text Text;
     public static bool melee_weapon_equiped = true;
     public static bool range_weapon_equiped = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        melee_weapon_equiped = true;
+        range_weapon_equiped = false;
+        Sword.gameObject.SetActive(true);
+        Bow.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -22,6 +27,7 @@ public class Weapons : MonoBehaviour
         {
             if(melee_weapon_equiped == true)
             {
+                Text.text = "Bow equiped!";
                 melee_weapon_equiped = false;
                 range_weapon_equiped = true;
                 Sword.gameObject.SetActive(false);
@@ -29,6 +35,7 @@ public class Weapons : MonoBehaviour
             }
             else if(range_weapon_equiped == true)
             {
+                Text.text = "Sword equiped!";
                 melee_weapon_equiped = true;
                 range_weapon_equiped = false;
                 Sword.gameObject.SetActive(true);
