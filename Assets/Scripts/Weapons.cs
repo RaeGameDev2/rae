1df6260c60cdfb9d1b86f1f08ebccc6247a9ed63
tanasescu-robert98 <80208723+tanasescu-robert98@users.noreply.github.com>
2021-07-23 12:21:50 +0300

    added logic to weapons switch

--- Assets/Scripts/Weapons.cs
+++ Assets/Scripts/Weapons.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class Weapons : MonoBehaviour
 {
-    public GameObject Player;
+    //public GameObject Player;
+    public GameObject Sword;
+    public GameObject Bow;
     public static bool melee_weapon_equiped = true;
     public static bool range_weapon_equiped = false;
     // Start is called before the first frame update
@@ -16,6 +18,22 @@ public class Weapons : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            if(melee_weapon_equiped == true)
+            {
+                melee_weapon_equiped = false;
+                range_weapon_equiped = true;
+                Sword.gameObject.SetActive(false);
+                Bow.gameObject.SetActive(true);
+            }
+            else if(range_weapon_equiped == true)
+            {
+                melee_weapon_equiped = true;
+                range_weapon_equiped = false;
+                Sword.gameObject.SetActive(true);
+                Bow.gameObject.SetActive(false);
+            }
+        }
     }
 }
