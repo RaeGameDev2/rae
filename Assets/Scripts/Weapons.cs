d4ad084e89ea2096dd46a08c356125979d190100
tanasescu-robert98 <80208723+tanasescu-robert98@users.noreply.github.com>
2021-07-24 17:12:30 +0300

    updated weapons script

--- Assets/Scripts/Weapons.cs
+++ Assets/Scripts/Weapons.cs
@@ -10,6 +10,9 @@ public class Weapons : MonoBehaviour
     public GameObject Bow;
     public static bool melee_weapon_equiped = true;
     public static bool range_weapon_equiped = false;
+    public int is_attacking = 0;
+    public int attack_timer = 700;
+    public string melee_atack_direction;
     void weapon_switch()
     {
         if (melee_weapon_equiped == true)
@@ -38,29 +41,55 @@ public class Weapons : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(is_attacking == 1)
+        {
+            attack_timer--;
+            Debug.Log(attack_timer);
+            if(attack_timer <= 0)
+            {
+                is_attacking = 0;
+                attack_timer = 700;
+            }
+        }
         if(melee_weapon_equiped == true)
         {
-            Sword.transform.position = new Vector2(Player.transform.position.x - 3, Player.transform.position.y + 1);
+            if(is_attacking == 0)
+                Sword.transform.position = new Vector2(Player.transform.position.x - 3, Player.transform.position.y + 1);
+            else
+            {
+                if(melee_atack_direction == "right")
+                    Sword.transform.position = new Vector2(Player.transform.position.x + 3, Player.transform.position.y + 1);
+                if (melee_atack_direction == "up")
+                    Sword.transform.position = new Vector2(Player.transform.position.x, Player.transform.position.y + 3);
+            }
         }
-        else
+        else if (range_weapon_equiped == true && is_attacking == 0)
         {
-            Bow.transform.position = new Vector2(Player.transform.position.x - 3, Player.transform.position.y + 1);
+            if (is_attacking == 0)
+                Bow.transform.position = new Vector2(Player.transform.position.x - 3, Player.transform.position.y + 1);
         }
+
         if (Input.GetKeyDown(KeyCode.T))
         {
             weapon_switch();
         }
-        if (Input.GetKey("left") && melee_weapon_equiped == true)
+        if (Input.GetKeyDown(KeyCode.J))
         {
             Sword.transform.position = new Vector2(Player.transform.position.x - 3, Player.transform.position.y + 1);
+            is_attacking = 1;
+            melee_atack_direction = "left";
         }
-        if (Input.GetKey("right") && melee_weapon_equiped == true)
+        if (Input.GetKeyDown(KeyCode.L))
         {
             Sword.transform.position = new Vector2(Player.transform.position.x + 3, Player.transform.position.y + 1);
+            is_attacking = 1;
+            melee_atack_direction = "right";
         }
-        if (Input.GetKey("up") && melee_weapon_equiped == true)
+        if (Input.GetKeyDown(KeyCode.I))
         {
             Sword.transform.position = new Vector2(Player.transform.position.x, Player.transform.position.y + 3);
+            is_attacking = 1;
+            melee_atack_direction = "up";
         }
     }
 }
