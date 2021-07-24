05ef42af0a8dcc7c7473590bb3d5f782b21619b2
adriandrag18 <66696874+adriandrag18@users.noreply.github.com>
2021-07-24 15:57:26 +0300

    imported PlayerCoontroller script from master

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,25 +1,76 @@
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
-    public float speed;
-    public float jumpForce;
-    private Rigidbody2D rb;
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpHeight;
 
-    void Start()
+    private Rigidbody2D rb;
+    private float hInput;
+
+    [HideInInspector] public bool grounded;
+    [HideInInspector] public bool isJumping;
+
+    public enum Direction
+    {
+        left,
+        right
+    }
+    private Direction direction;
+
+    private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-        speed = 10;
-        jumpForce = 10;
+        direction = Direction.right;
+        isJumping = false;
+        grounded = true;
     }
 
-    void Update()
+    //Polls input every frame and updates flags accordingly
+    private void HandleInput()
     {
-        var x = Input.GetAxis("Horizontal");
+        hInput = Input.GetAxis("Horizontal");
 
-        transform.position += new Vector3(x, 0) * speed * Time.deltaTime;
-        if (Input.GetButtonDown("Jump"))
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        if (Input.GetKey(KeyCode.Space) && grounded)
+            isJumping = true;
+    }
+
+    //Flip player when changing direction
+    private void ChangeDirection()
+    {
+        if (hInput > 0.01f)
+        {
+            if (direction == Direction.left)
+            {
+                transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+                direction = Direction.right;
+            }
+
+        }
+        else if (hInput < -0.01f)
+        {
+            if (direction == Direction.right)
+            {
+                transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+                direction = Direction.left;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        HandleInput();
+        ChangeDirection();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(hInput * speed, rb.velocity.y);
+
+        if (isJumping && grounded)
+        {
+            rb.AddForce(new Vector2(rb.velocity.x, jumpHeight), ForceMode2D.Impulse);
+
+        }
     }
 }
