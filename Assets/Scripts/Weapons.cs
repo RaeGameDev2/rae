4d2a7df0bda8890403c23fa7d1fbb41fb4428902
tanasescu-robert98 <80208723+tanasescu-robert98@users.noreply.github.com>
2021-07-24 14:28:32 +0300

    made weapons follow the player

--- Assets/Scripts/Weapons.cs
+++ Assets/Scripts/Weapons.cs
@@ -5,16 +5,29 @@ using UnityEngine.UI;
 
 public class Weapons : MonoBehaviour
 {
-    //public GameObject Player;
-    public ParticleSystem expl;
-    public ParticleSystem expl1;
-    public ParticleSystem expl2;
+    public GameObject Player;
     public GameObject Sword;
     public GameObject Bow;
-    public Text Text;
-    public GameObject Player;
     public static bool melee_weapon_equiped = true;
     public static bool range_weapon_equiped = false;
+    void weapon_switch()
+    {
+        if (melee_weapon_equiped == true)
+        {
+            melee_weapon_equiped = false;
+            range_weapon_equiped = true;
+            Sword.gameObject.SetActive(false);
+            Bow.gameObject.SetActive(true);
+        }
+        else if (range_weapon_equiped == true)
+        {
+            melee_weapon_equiped = true;
+            range_weapon_equiped = false;
+            Sword.gameObject.SetActive(true);
+            Bow.gameObject.SetActive(false);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,35 +36,19 @@ public class Weapons : MonoBehaviour
     }
 
     // Update is called once per frame
-    [System.Obsolete]
     void Update()
     {
+        if(melee_weapon_equiped == true)
+        {
+            Sword.transform.position = new Vector2(Player.transform.position.x - 3, Player.transform.position.y + 1);
+        }
+        else
+        {
+            Bow.transform.position = new Vector2(Player.transform.position.x - 3, Player.transform.position.y + 1);
+        }
         if (Input.GetKeyDown(KeyCode.T))
         {
-            if(melee_weapon_equiped == true)
-            {
-                expl1 = Instantiate(expl, new Vector3(0,0,0), Quaternion.identity);
-                expl2 = Instantiate(expl, new Vector3(0, 0, 0), Quaternion.identity);
-                Text.text = "Bow equipped!";
-                melee_weapon_equiped = false;
-                range_weapon_equiped = true;
-                Sword.gameObject.SetActive(false);
-                Bow.gameObject.SetActive(true);
-                Destroy(expl1, expl1.duration + expl1.startLifetime);
-                Destroy(expl2, expl2.duration + expl2.startLifetime);
-            }
-            else if(range_weapon_equiped == true)
-            {
-                expl1 = Instantiate(expl, new Vector3(0, 0, 0), Quaternion.identity);
-                expl2 = Instantiate(expl, new Vector3(0, 0, 0), Quaternion.identity);
-                Text.text = "Sword equipped!";
-                melee_weapon_equiped = true;
-                range_weapon_equiped = false;
-                Sword.gameObject.SetActive(true);
-                Bow.gameObject.SetActive(false);
-                Destroy(expl1, expl1.duration + expl1.startLifetime);
-                Destroy(expl2, expl2.duration + expl2.startLifetime);
-            }
+            weapon_switch();
         } 
     }
 }
