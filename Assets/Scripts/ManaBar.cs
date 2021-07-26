982586da60fd84642b46d7a9729cefcd261e8530
Feldron21 <adrian_mihai.jercan@stud.acs.upb.ro>
2021-07-26 19:03:32 +0300

    -added ManaBar with Mana Regen

---
+++ Assets/Scripts/ManaBar.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManaBar : MonoBehaviour
+{
+    public Slider slider;
+
+    public void SetMana(float mana)
+    {
+        slider.value = mana;
+    }
+
+    public void SetMaxMana(float mana)
+    {
+        slider.maxValue = mana;
+        slider.value = mana;
+    }
+
+}
--- Assets/Scripts/Resources.cs
+++ Assets/Scripts/Resources.cs
@@ -6,20 +6,30 @@ using UnityEngine.UI;
 public class Resources : MonoBehaviour
 {
     public int maxHealth = 100;
-    public int currentHealth = 0;
+    public int currentHealth;
+    public float maxMana = 100;
+    public float currentMana;
+    public float manaAmount = 1f;
+   
 
     public HealthBar healthBar;
+    public ManaBar manaBar;
 
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
         healthBar.SetMaxHealth(maxHealth);
+
+        currentMana = maxMana;
+        manaBar.SetMaxMana(maxMana);
     }
 
     // Update is called once per frame
     void Update()
     {
+        NormalizeMana();
+
         if(Input.GetKeyDown(KeyCode.Tab))
         {
             if (currentHealth > 0)
@@ -27,12 +37,39 @@ public class Resources : MonoBehaviour
                 TakeDamage(20);
             }
             else currentHealth = 0;
+
+
         }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            if (currentMana > 0)
+            {
+                TakeMana(20);
+            }
+            else currentMana = 0;
+        }
+       
+        
     }
 
     void TakeDamage(int damage)
     {
         currentHealth -= damage;
         healthBar.SetHealth(currentHealth);
+    }
+
+    void TakeMana(float mana)
+    {
+        currentMana -= mana;
+        manaBar.SetMana(currentMana);
+    }
+    void NormalizeMana()
+    {
+        if (currentMana < 100)
+        {
+            currentMana += manaAmount * Time.deltaTime;
+            manaBar.SetMana(currentMana);
+        }
+        else currentMana = 100;
     }
 }
