16a2b1dde3b4b58b6bad761b9a26450852d24f58
Badea Andrei <baditaandrei20@gmail.com>
2021-07-26 17:17:01 +0300

    - Resources added + HealthBar
    Am o problema la health bar: in momentul in  care se apasa tasta tab viata scade, dar nu se observa pe slider decat cand viata ajunge la 0.

--- Assets/Scripts/Resources.cs
+++ Assets/Scripts/Resources.cs
@@ -5,26 +5,30 @@ using UnityEngine.UI;
 
 public class Resources : MonoBehaviour
 {
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if(Input.GetKeyDown(KeyCode.Tab))
+        {
+            TakeDamage(20);
+        }
     }
 
-    void decreaseMana(float value)
+    void TakeDamage(int damage)
     {
-
+        currentHealth -= damage;
+        healthBar.SetMaxHealth(currentHealth);
     }
-
-    void decreaseHP(float hp)
-    {
-
-    }
-
 }
