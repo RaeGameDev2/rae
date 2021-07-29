ec1dac98171c1e0350bde00431489a176059ae53
Anca Buzatu <58979939+ancamariabuzatu@users.noreply.github.com>
2021-07-29 18:21:09 +0300

    - parallax effect

--- Assets/Scripts/ParallaxEffect.cs
+++ Assets/Scripts/ParallaxEffect.cs
@@ -2,29 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class SpritesData
+{
+   public SpriteRenderer spriteObj;
+   public int layer = 0;
+   [Range(0.0f, 1.0f)]
+    public float parallaxEffect = 1.0f;
+}
+
 public class ParallaxEffect : MonoBehaviour
 {
-    private float length;
-    private Vector3 startPosition;
+    private float[] length;
+    private Vector3[] startPosition;
     public GameObject gameCamera;
-    public float parallaxEffect;
+    [Tooltip("Add background sprites")]
+    public SpritesData[] spritesList;
     // Start is called before the first frame update
     void Start()
     {
-        startPosition = transform.position;
-        length = GetComponent<SpriteRenderer>().bounds.size.x;
+        length = new float[spritesList.Length];
+        startPosition = new Vector3[spritesList.Length];
+        for (int i = 0; i < spritesList.Length; i++) {
+            startPosition[i] = spritesList[i].spriteObj.transform.position;
+            length[i] = spritesList[i].spriteObj.bounds.size.x;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        float temp = gameCamera.transform.position.x * (1 - parallaxEffect);
-        float dist = gameCamera.transform.position.x * parallaxEffect;
-        transform.position = new Vector3(startPosition[0] + dist, startPosition[1], startPosition[2]);
-    
-        if (temp > startPosition[0] + (length / 2))
-            startPosition[0] += length;
-        else if (temp < startPosition[0] - (length /2))
-                startPosition[0] -= length;
+        int i = 0;
+        foreach (SpritesData spData in spritesList) {
+            float temp = gameCamera.transform.position.x * (1 - spData.parallaxEffect);
+            float dist = gameCamera.transform.position.x * spData.parallaxEffect;
+            spData.spriteObj.sortingOrder = spData.layer;
+            spData.spriteObj.transform.position = new Vector3(startPosition[i].x + dist, startPosition[i].y, startPosition[i].z);
+
+            if (temp > startPosition[i].x + (length[i] / 2))
+                startPosition[i].x += length[i];
+            else if (temp < startPosition[i].x - (length[i] /2))
+                startPosition[i].x -= length[i];
+            i++;
+        }
     }
 }
