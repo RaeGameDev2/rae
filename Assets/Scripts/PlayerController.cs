3cda44b64d5b4ab7cdd96915853222ca69d58bfe
Andrei-Daniel Voicu <saitatter133@gmail.com>
2021-08-02 01:49:11 +0300

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,39 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float speed;
+
+    [SerializeField] private float groundSpeed;
+    [SerializeField] private float airSpeed;
+
     [SerializeField] private float jumpHeight;
+    [SerializeField] private float jumpSpeed;
+    [SerializeField] private float fallSpeed;
+    [SerializeField] private float gravity;
+
+    [SerializeField] private float dashSpeed;
+    [SerializeField] private float dashDistance;
+    [SerializeField] private float dashCooldown;
+
+    private float currGravity;
+    private float prevVelocityY;
+
 
     private Rigidbody2D rb;
     private float hInput;
 
-    [HideInInspector] public bool grounded;
-    [HideInInspector] public bool isJumping;
+    [HideInInspector] public bool isGrounded;
+    [HideInInspector] public bool canJump;
+    [HideInInspector] public bool canDoubleJump;
+    [HideInInspector] public bool diagonalJump;
+
+    private bool jumpPressed;
+    private bool dashPressed;
+    private float distanceTraveled;
+    private float timeSinceDash;
+
+    private bool isDashing;
 
     public enum Direction
     {
@@ -22,23 +46,31 @@ public class PlayerController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         direction = Direction.right;
-        isJumping = false;
-        grounded = true;
+        canJump = false;
+        diagonalJump = false;
+        isGrounded = true;
+        currGravity = gravity;
+        canDoubleJump = false;
+        prevVelocityY = 0;
+        isDashing = false;
+        distanceTraveled = 0;
+        timeSinceDash = dashCooldown;
     }
 
     //Polls input every frame and updates flags accordingly
     private void HandleInput()
     {
-        hInput = Input.GetAxis("Horizontal");
-
-        if (Input.GetKey(KeyCode.Space) && grounded)
-            isJumping = true;
+        hInput = Input.GetAxisRaw("Horizontal");
+        if (Input.GetKeyDown(KeyCode.Space))
+            jumpPressed = true;
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+            dashPressed = true;
     }
 
     //Flip player when changing direction
     private void ChangeDirection()
     {
-        if (hInput > 0.01f)
+        if (hInput > 0.01f && rb.velocity.x > 0)
         {
             if (direction == Direction.left)
             {
@@ -47,7 +79,7 @@ public class PlayerController : MonoBehaviour
             }
 
         }
-        else if (hInput < -0.01f)
+        else if (hInput < -0.01f && rb.velocity.x < 0)
         {
             if (direction == Direction.right)
             {
@@ -61,16 +93,107 @@ public class PlayerController : MonoBehaviour
     {
         HandleInput();
         ChangeDirection();
+        if (isDashing == false)
+        {
+            timeSinceDash += Time.deltaTime;
+        }
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+        currGravity = jumpSpeed * jumpSpeed / (2 * jumpHeight);
+        if (rb.velocity.x != 0)
+            diagonalJump = true;
+    }
+
+    private IEnumerator Dash()
+    {
+        Vector2 initPos = rb.position;
+        distanceTraveled = 0;
+
+        while (distanceTraveled < dashDistance)
+        {
+            distanceTraveled += dashSpeed * Time.fixedDeltaTime;
+            Debug.Log(distanceTraveled);
+            yield return new WaitForFixedUpdate();
+        }
+        if (direction == Direction.right)
+            rb.position = new Vector2(initPos.x + dashDistance, initPos.y);
+        else
+            rb.position = new Vector2(initPos.x - dashDistance, initPos.y);
+        isDashing = false;
+        timeSinceDash = 0;
+    }
+
+    private void CheckJump()
+    {
+        if (jumpPressed)
+        {
+            jumpPressed = false;
+
+            if (isGrounded)
+            {
+                Jump();
+                canDoubleJump = true;
+
+            }
+            else if (canDoubleJump)
+            {
+                Jump();
+                canDoubleJump = false;
+            }
+        }
     }
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(hInput * speed, rb.velocity.y);
+        //Reset gravity to default values
+        if (isGrounded || rb.velocity.y < 0)
+            currGravity = gravity;
 
-        if (isJumping && grounded)
+        //If not in dash, handles jump
+        if (!isDashing)
         {
-            rb.AddForce(new Vector2(rb.velocity.x, jumpHeight), ForceMode2D.Impulse);
+            CheckJump();
 
+            if (isGrounded)
+                rb.velocity = new Vector2(hInput * groundSpeed, rb.velocity.y);
+            else
+            {
+                float speed = hInput * airSpeed + rb.velocity.x;
+                if (diagonalJump)
+                    speed = Mathf.Clamp(speed, -groundSpeed, groundSpeed);
+                else
+                    speed = Mathf.Clamp(speed, -airSpeed, airSpeed);
+                if (rb.velocity.y == 0 || (rb.velocity.y < 0 && prevVelocityY > 0))
+                {
+                    rb.velocity = new Vector2(speed, -fallSpeed);
+
+                }
+                rb.velocity = new Vector2(speed, rb.velocity.y);
+            }
+            //Add simulated gravity
+            rb.AddForce(currGravity * Vector2.down, ForceMode2D.Force);
         }
+
+        if (dashPressed && !isDashing && timeSinceDash >= dashCooldown)
+        {
+            dashPressed = false;
+            isDashing = true;
+            StartCoroutine("Dash");
+            if (direction == Direction.right)
+            {
+                rb.velocity = new Vector2(dashSpeed, 0);
+            }
+            else
+            {
+                rb.velocity = new Vector2(-dashSpeed, 0);
+            }
+        }
+
+        //Save previous y-velocity for adding fallSpeed
+        prevVelocityY = rb.velocity.y;
+        //  Debug.Log(rb.velocity);
     }
 }
