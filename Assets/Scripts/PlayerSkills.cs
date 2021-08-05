148e9623d3f4e8dac171c48087d6bf35a023d2c4
adriandrag18 <66696874+adriandrag18@users.noreply.github.com>
2021-08-06 00:24:05 +0300

    all methods in PlayerSkills

--- Assets/Scripts/PlayerSkills.cs
+++ Assets/Scripts/PlayerSkills.cs
@@ -46,9 +46,76 @@ public class PlayerSkills : MonoBehaviour
     }
 
     //Exemplu:
+    public bool IsLifeDrainUnlocked()
+    {
+        return playerSkills.IsSkillUnlocked(Skills.SkillType.LifeDrain);
+    }
     public bool IsParryUnlocked()
     {
-         return playerSkills.IsSkillUnlocked(Skills.SkillType.Parry);
+        return playerSkills.IsSkillUnlocked(Skills.SkillType.Parry);
     }
-
+    public bool IsLifeQuickTpUnlocked()
+    {
+        return playerSkills.IsSkillUnlocked(Skills.SkillType.QuickTP);
+    }
+    public bool IsLifeShieldUnlocked()
+    {
+        return playerSkills.IsSkillUnlocked(Skills.SkillType.Shield);
+    }
+    public bool IsLifePhaseWalkUnlocked()
+    {
+        return playerSkills.IsSkillUnlocked(Skills.SkillType.PhaseWalk);
+    }
+    public bool IsLifeDebuffUnlocked()
+    {
+        return playerSkills.IsSkillUnlocked(Skills.SkillType.Debuff);
+    }
+    public int GetLevelParry()
+    {
+        return playerSkills.GetSkillLevel(Skills.SkillType.Parry);
+    }
+    public int GetLevelLifeDrain()
+    {
+        return playerSkills.GetSkillLevel(Skills.SkillType.LifeDrain);
+    }
+    public int GetLevelQuickTP()
+    {
+        return playerSkills.GetSkillLevel(Skills.SkillType.QuickTP);
+    }
+    public int GetLevelShield()
+    {
+        return playerSkills.GetSkillLevel(Skills.SkillType.Shield);
+    }
+    public int GetLevelPhaseWalk()
+    {
+        return playerSkills.GetSkillLevel(Skills.SkillType.PhaseWalk);
+    }
+    public int GetLevelDebuff()
+    {
+        return playerSkills.GetSkillLevel(Skills.SkillType.Debuff);
+    }
+    public int GetLevelAttack()
+    {
+        return playerSkills.GetSkillLevel(Skills.SkillType.Attack);
+    }
+    public int GetLevelCritBonus()
+    {
+        return playerSkills.GetSkillLevel(Skills.SkillType.CritBonus);
+    }
+    public int GetLevelCritRate()
+    {
+        return playerSkills.GetSkillLevel(Skills.SkillType.CritRate);
+    }
+    public int GetLevelAttackSpeed()
+    {
+        return playerSkills.GetSkillLevel(Skills.SkillType.AttackSpeed);
+    }
+    public int GetLevelDefense()
+    {
+        return playerSkills.GetSkillLevel(Skills.SkillType.Defense);
+    }
+    public int GetLevelSize()
+    {
+        return playerSkills.GetSkillLevel(Skills.SkillType.Size);
+    }
 }
