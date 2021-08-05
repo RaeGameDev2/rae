ed9668049d45dc31c93d10071bba55560147b012
CostinTudor <87725310+CostinTudor@users.noreply.github.com>
2021-08-05 19:15:53 +0300

    Add files via upload

--- Assets/Scripts/Skills.cs
+++ Assets/Scripts/Skills.cs
@@ -34,6 +34,12 @@ public class Skills
     {
         if (SkillLevel[(int)skillType] == MAX_LEVEL) return;
         if (SkillPoints == 0) return;
+        if (skillType == SkillType.Parry && SkillLevel[(int)SkillType.LifeDrain] > 0) return;
+        if (skillType == SkillType.LifeDrain && SkillLevel[(int)SkillType.Parry] > 0) return;
+        if (skillType == SkillType.QuickTP && SkillLevel[(int)SkillType.Shield] > 0) return;
+        if (skillType == SkillType.Shield && SkillLevel[(int)SkillType.QuickTP] > 0) return;
+        if (skillType == SkillType.PhaseWalk && SkillLevel[(int)SkillType.Debuff] > 0) return;
+        if (skillType == SkillType.Debuff && SkillLevel[(int)SkillType.PhaseWalk] > 0) return;
         SkillLevel[(int)skillType]++;
         SkillPoints--;
     }
