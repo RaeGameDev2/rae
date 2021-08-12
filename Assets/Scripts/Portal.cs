3e2572421ab4c4a3d6428fc412a9c35d432f211d
adriandrag18 <66696874+adriandrag18@users.noreply.github.com>
2021-08-12 03:39:02 +0300

    hubWord

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -1,34 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Portal : MonoBehaviour
 {
-    public static bool player_is_here = false;
-    // Start is called before the first frame update
-    void Start()
+    private GameObject fireButton;
+    private GameObject iceButton;
+    private GameObject interiorPortal;
+
+    private void Start()
     {
-        
+        fireButton = GameObject.Find("Fire_Button");
+        iceButton = GameObject.Find("Ice_Button");
+        interiorPortal = GameObject.Find("Interior Portal");
+        fireButton.SetActive(false);
+        iceButton.SetActive(false);
+        interiorPortal.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Fire_Load()
     {
-        
+        SceneManager.LoadScene(1);
+    }
+
+    public void Ice_Load()
+    {
+        SceneManager.LoadScene(2);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag == "Player")
-        {
-            player_is_here = true;
-        }
+        if (collision.tag != "Player") return;
+        fireButton.SetActive(true);
+        iceButton.SetActive(true);
+        interiorPortal.SetActive(true);
     }
+
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.tag == "Player")
-        {
-            player_is_here = false;
-        }
+        if (collision.tag != "Player") return;
+        fireButton.SetActive(false);
+        iceButton.SetActive(false);
+        interiorPortal.SetActive(false);
     }
 }
