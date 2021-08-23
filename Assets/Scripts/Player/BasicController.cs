da5ac953bd906486c16528002f3ec047325d696e
Andrei-Daniel Voicu <saitatter133@gmail.com>
2021-08-24 00:13:29 +0300

    - made hub movement without acceleration so it simulates realm-world movement

--- Assets/Scripts/Player/BasicController.cs
+++ Assets/Scripts/Player/BasicController.cs
@@ -12,22 +12,22 @@ public class BasicController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         anim.SetInteger("weapon", 2);
         anim.SetInteger("type", 0);
-        anim.SetInteger("state", (int) AnimState.Idle);
+        anim.SetInteger("state", (int)AnimState.Idle);
         anim.SetFloat("speed", 1f);
     }
 
     private void Update()
     {
-        var x = Input.GetAxis("Horizontal");
+        var x = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(x * speed, rb.velocity.y);
         if (x == 0f)
         {
-            anim.SetInteger("state", (int) AnimState.Idle);
+            anim.SetInteger("state", (int)AnimState.Idle);
         }
         else
         {
             transform.localScale = new Vector3(x < 0f ? -1f : 1f, transform.localScale.y, transform.localScale.z);
-            anim.SetInteger("state", (int) AnimState.Run);
+            anim.SetInteger("state", (int)AnimState.Run);
             anim.SetFloat("speed", rb.velocity.x / 4f);
         }
     }
