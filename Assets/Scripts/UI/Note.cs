4a94f3bd9fedba9bb8701070befc7914be25cef2
Andrei-Daniel Voicu <saitatter133@gmail.com>
2021-08-24 21:29:44 +0300

    - imported note sprite and finished note script

--- Assets/Scripts/UI/Note.cs
+++ Assets/Scripts/UI/Note.cs
@@ -5,12 +5,41 @@ using UnityEngine;
 public class Note : MonoBehaviour
 {
     public Sprite UISprite;
+    private UnityEngine.UI.Image interactIcon;
+    private UnityEngine.UI.Image noteImage;
+    bool nearby;
 
+    private void Start()
+    {
+        interactIcon = GameObject.Find("InteractIcon").GetComponent<UnityEngine.UI.Image>();
+        noteImage = GameObject.Find("NoteImage").GetComponent<UnityEngine.UI.Image>();
+        nearby = false;
+    }
+    private void Update()
+    {
+        if (nearby && Input.GetKeyDown("e"))
+        {
+            noteImage.enabled = !noteImage.enabled;
+            noteImage.sprite = UISprite;
+        }
+
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            Debug.Log("aici e note");
+            nearby = true;
+            interactIcon.enabled = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            interactIcon.enabled = false;
+            noteImage.enabled = false;
+            nearby = false;
         }
     }
 }
